2b686d2f9a66623bed7a4753fa1a1e46c5c681d3
daniel-espindola <espindola.daniel@aluno.ufabc.edu.br>
2019-08-18 16:38:39 -0300

    Delete 501.py

--- 501.py
+++
@@ -1,9 +0,0 @@
-primos = open("cousins.txt",'r')
-
-primos = primos.read()
-primos = primos.replace("\n", "")
-
-primos = primos.split(" ")
-
-primos = [int(n) for n in primos if n!=""]
-print(primos)
